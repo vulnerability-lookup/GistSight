191637a03e88e77fa96a1e7de0a2a0b1187d7563
Cédric Bonhomme <cedric@cedricbonhomme.org>
2025-01-06 23:46:28 +0100

    chg: set example token to empty string.

--- gistsight/conf_sample.py
+++ gistsight/conf_sample.py
@@ -4,4 +4,4 @@ github_api_url = "https://api.github.com/graphql"
 github_token = "your_github_token"
 
 vulnerability_lookup_base_url = "http://127.0.0.1:5000/"
-vulnerability_auth_token = "qoBZi2sv6v6BRRRoxsMl2vbgesT6bT7Vl0zVUia1OHYi-xj5BM3JsaLbfL0eAd5P_ByV3r17nE3jY-xYusWzXA"
+vulnerability_auth_token = ""
